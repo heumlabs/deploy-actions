be1b460c87977fc425596c3e1690cfef04758898
jongsoo.bae <js.bae@heumlabs.io>
2022-08-24 15:00:03 +0900

    add target

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,10 @@ from fabric import task
 
 
 @task
-def create_r2m(ctx, repo, branch, title):
+def create_r2m(ctx, repo, branch, target, title):
     response = requests.post("https://deploy.heumtax.com/deployment/", json={
-        "repo": repo, "branch": branch, "title": title
+        "repo": repo, "branch": branch, "target": target, "title": title
     })
-    print(repo, branch, title)
+    print(repo, branch, target, title)
     print(response.ok)
     print(response.content)
