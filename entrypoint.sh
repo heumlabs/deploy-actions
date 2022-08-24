ff57c0f5b8fd79b04bf618491c49efaabc2512aa
jongsoo.bae <js.bae@heumlabs.io>
2022-08-24 13:29:43 +0900

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-fab create_r2m -r $1 -b $2 -t $3
+fab create-r2m -r $1 -b $2 -t $3
--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ from fabric import task
 
 @task
 def create_r2m(ctx, repo, branch, title):
-    requests.post("https://deploy.heumtax.com/deploy/", json={
+    response = requests.post("https://deploy.heumtax.com/deploy/", json={
         "repo": repo, "branch": branch, "title": title
     })
+    print(response.ok)
